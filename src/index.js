125cfd0d9521af002d0deee25bb71da395f07f8b
annchapon <annchapon@gmail.com>
2020-10-29 18:08:02 +0300

    add react components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search" />
+const TodoList = () => {
+  return (
     <ul>
       <li>Drink tea</li>
       <li>Drink coffee</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />    
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
