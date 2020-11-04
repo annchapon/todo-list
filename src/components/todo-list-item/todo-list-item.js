c5b0054b599f079591bc3a844d0339682af04bca
annchapon <annchapon@gmail.com>
2020-11-04 11:43:14 +0300

    add function onLabelClick

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,18 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+    /*constructor() {
+        super();
+
+        this.onLabelClick = () => {
+            console.log(`Done: ${this.props.label}`);
+        }
+    }*/
+
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
+    }    
+
     render() {
         const { label, important = false } = this.props;
 
@@ -13,7 +25,10 @@ export default class TodoListItem extends Component {
     
         return (
             <span className="todo-list-item">
-                <span className="todo-list-item-label" style={style}>
+                <span 
+                    className="todo-list-item-label" 
+                    style={style}
+                    onClick={ this.onLabelClick } >
                     {label}
                 </span>
                 <button type="button" className="btn btn-outline-success btn-sm float-right">
